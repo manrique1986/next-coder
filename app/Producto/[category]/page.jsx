ec8db867db0cc197be3300b5de354a9881c477e4
Leandro Martini <leandromartini86@gmail.com>
2024-09-08 17:06:24 -0300

    probando deploy

--- app/Producto/[category]/page.jsx
+++ app/Producto/[category]/page.jsx
@@ -1,22 +1,22 @@
-"use client";
+// "use client";
 
-import React from "react";
-import { productos } from "@/Data/mockData"; 
-import { useParams } from "next/navigation";
-import ProductList from "@/components/ProductList";
+// import React from "react";
+// import { productos } from "@/Data/mockData"; 
+// import { useParams } from "next/navigation";
+// import ProductList from "@/components/ProductList";
 
-const Page = () => {
-  const { category } = useParams(); 
+// const Page = () => {
+//   const { category } = useParams(); 
 
 
-  const filteredProducts = category === "all" ? productos : productos.filter(item => item.category.toUpperCase() === category.toUpperCase());
+//   const filteredProducts = category === "all" ? productos : productos.filter(item => item.category.toUpperCase() === category.toUpperCase());
 
-  return (
-    <div>
-      <h1>Category: {category}</h1>
-      <ProductList productos={filteredProducts} />
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <h1>Category: {category}</h1>
+//       <ProductList productos={filteredProducts} />
+//     </div>
+//   );
+// };
 
-export default Page;
+// export default Page;
